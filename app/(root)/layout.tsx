a2ce8588455f8343d8ca9282820da43dd59416ae
-global <-global>
2025-07-08 20:36:25 +0500

    commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Children } from '../../type'
 import Navbar from './_components/navbar'
-import { ThemeProvider } from "@/components/theme-provider"
+
 const Layout = ({children} :Children) => {
   return (
     <div>
