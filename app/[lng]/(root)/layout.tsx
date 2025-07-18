9720216f9896cc16172c04eedf25820de9e05b5d
-global <-global>
2025-07-18 16:29:51 +0500

    szas

--- app/[lng]/(root)/layout.tsx
+++ app/[lng]/(root)/layout.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Children } from '../../../type'
 import Navbar from './_components/navbar'
-import Hero from './(home)/_comonents/hero'
-import Mobile from '@/components/shared/mobile'
+
+
 
 const Layout = ({children} :Children) => {
   return (
